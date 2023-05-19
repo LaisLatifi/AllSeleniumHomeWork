6dc749140d329ac2d077b1f5a88992441d8f1927
LaisLatifi <mudaser1992@gmail.com>
2023-05-19 11:28:58 -0700

    HW02

--- src/test/java/Class1/HW1.java
+++ src/test/java/Class1/HW1.java
@@ -1,4 +1,40 @@
 package Class1;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HW1 {
-}
+    public static void main(String[] args) {
+
+        WebDriver driver=new ChromeDriver();
+
+         /*
+        HW1:
+navigate to https://parabank.parasoft.com/parabank/register.htm;jsessionid=B8DD31FF0AFEDBDF4454B27BBC5C05B0%22
+fill out the form
+close the browser
+[19:18]
+Note: use name or id as locators
+         */
+
+        driver.get(" https://parabank.parasoft.com/parabank/register.htm;jsessionid=B8DD31FF0AFEDBDF4454B27BBC5C05B0%22");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("customer.firstName")).sendKeys("Lais M");
+        driver.findElement(By.id("customer.lastName")).sendKeys("latifi");
+        driver.findElement(By.id("customer.address.street")).sendKeys("4451 Gate Way park");
+        driver.findElement(By.id("customer.address.city")).sendKeys("Bay Area");
+        driver.findElement(By.id("customer.address.state")).sendKeys("California");
+        driver.findElement(By.id("customer.address.zipCode")).sendKeys("95959");
+        driver.findElement(By.id("customer.phoneNumber")).sendKeys("123-456-7890");
+        driver.findElement(By.id("customer.ssn")).sendKeys("123-4567-34");
+        driver.findElement(By.id("customer.username")).sendKeys("LaisM");
+        driver.findElement(By.id("customer.password")).sendKeys("itIsPassword");
+        driver.findElement(By.id("repeatedPassword")).sendKeys("confirmPassword");
+        driver.quit();
+
+
+
+    }
+    }
+
--- src/test/java/Class1/HW2.java
+++ src/test/java/Class1/HW2.java
@@ -1,4 +1,30 @@
 package Class1;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HW2 {
+    /*
+       HW2:
+   navigate to "http://secure.smartbearsoftware.com/samples/testcomplete11/WebOrders/login.aspx"
+   enter the username "Tester"
+   enter the password "test"
+   get the title of the webPage and confirm that it is  "Web Orders Login"
+        */
+    public static void main(String[] args) {
+
+        WebDriver driver=new ChromeDriver();
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete11/WebOrders/login.aspx");
+        driver.findElement(By.name("ctl00$MainContent$username")).sendKeys("Tester");
+        driver.findElement(By.name("ctl00$MainContent$password")).sendKeys("test");
+        String title=driver.getTitle();
+        if (title.equals("Web Orders Login")){
+            System.out.println("Title confirmed");
+        }else {
+            System.out.println("Title not confirmed");
+        }
+
+
+    }
 }
